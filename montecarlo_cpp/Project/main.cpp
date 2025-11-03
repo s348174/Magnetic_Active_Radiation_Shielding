6500097ff4d9cc12441af646a12940fe17ff191c
alberto <prinoalberto02@gmail.com>
2025-11-03 16:25:25 +0100

    Added simulation time measurement

--- montecarlo_cpp/Project/main.cpp
+++ montecarlo_cpp/Project/main.cpp
@@ -4,17 +4,19 @@
 #include <Torus.hpp>
 #include <Particle.hpp>
 #include <Utils.hpp>
+#include <chrono>
 
 using namespace std;
 using namespace Eigen;
 
 int main()
 {
+    chrono::steady_clock::time_point t_begin = chrono::steady_clock::now();
     // Simulation arguments
-    double N = 1000;
+    double N = 1e4;
     const double T = 1e7; // K
     double dt = 1e-9;
-    unsigned long seed = 1002;
+    unsigned long seed = 10000;
 
     // Define Torus
     double R = 20;
@@ -25,9 +27,11 @@ int main()
     torus.rho = rho;
     torus.I = I;
 
+    // Run multithread simulation from CSV input
     runFromCSV_MT("../Project/particles_input.csv", torus, N, T, dt, seed);
 
-    // if (monteCarlo(torus, m, q, N, T, dt))
-    //     cout << "Simulation completed successfully!" << endl;
+    chrono::steady_clock::time_point t_end = chrono::steady_clock::now();
+    double elapsedTime = chrono::duration_cast<chrono::milliseconds>(t_end-t_begin).count();
+    cout << "Elapsed simulation time: " << elapsedTime << "ms." << endl;
     return 0;
 }
