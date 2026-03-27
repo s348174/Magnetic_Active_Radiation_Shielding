d848423342a46b87cef5025835919b0954c1809c
sbornik <s348174@studenti.polito.it>
2026-03-27 11:19:08 +1100

    Deleted build folders from tracking and created BO structrue

--- montecarlo_cpp/3rd_party_libraries/build/Main_Build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alberto/Documents/Magnetic_Active_Radiation_Shielding/montecarlo_cpp/3rd_party_libraries/build/Main_Source/googletest/googletest
-# Build directory: /home/alberto/Documents/Magnetic_Active_Radiation_Shielding/montecarlo_cpp/3rd_party_libraries/build/Main_Build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
